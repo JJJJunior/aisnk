20bc30a49bf94333e54629d5bbf01cac0369a630
Maxwell <xj.master@gmail.com>
2024-09-28 14:46:16 +0800

    调整统计数据时区

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,104 +5,99 @@ import TotalBar from "../components/main/TotalBar";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 
 const AdminPage = async () => {
-  const utcOffset = 8; // +8 GMT 时区偏移量
-
-  // 当前时间的 UTC 时间
+  // 获取当前时间的 UTC 时间
   const now = new Date();
 
-  // 计算今天开始的 UTC 时间
-  const startOfToday = new Date();
-  startOfToday.setUTCHours(0 - utcOffset, 0, 0, 0);
+  // 计算今天 UTC 0 点的时间
+  const startOfToday = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
 
-  // 计算昨天的 UTC 开始时间
-  const startOfYesterday = new Date(startOfToday);
-  startOfYesterday.setUTCDate(startOfYesterday.getUTCDate() - 1); // 昨天 0 点
+  // 计算昨天 UTC 0 点的时间
+  const startOfYesterday = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 1));
 
-  // 计算前天的 UTC 开始时间
-  const startOfDayBeforeYesterday = new Date(startOfYesterday);
-  startOfDayBeforeYesterday.setUTCDate(startOfDayBeforeYesterday.getUTCDate() - 1); // 前天 0 点
+  // 计算前天 UTC 0 点的时间
+  const startOfDayBeforeYesterday = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 2));
 
-  // 计算昨天的 UTC 结束时间
-  const endOfYesterday = new Date(startOfToday);
-  endOfYesterday.setUTCSeconds(-1); // 昨天 23:59:59
+  // 计算昨天的 UTC 结束时间（23:59:59）
+  const endOfYesterday = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()) - 1);
 
-  // 计算前天的 UTC 结束时间
-  const endOfDayBeforeYesterday = new Date(startOfYesterday);
-  endOfDayBeforeYesterday.setUTCSeconds(-1); // 前天 23:59:59
+  // 计算前天的 UTC 结束时间（23:59:59）
+  const endOfDayBeforeYesterday = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 1) - 1);
 
-  //客户数
-  const todayCustomers = await prisma.customer.findMany({
+  // 获取昨天的客户数
+  const yesterdayCustomers = await prisma.customer.findMany({
     where: {
       createdAt: {
-        gte: startOfYesterday, // 昨天 0 点（+8）对应的 UTC 开始时间
-        lte: endOfYesterday, // 昨天 23:59:59 对应的 UTC 结束时间
+        gte: startOfYesterday, // 昨天 UTC 0 点
+        lte: endOfYesterday, // 昨天 UTC 23:59:59
       },
     },
   });
-  const yesdayCustomers = await prisma.customer.findMany({
+
+  // 获取前天的客户数
+  const dayBeforeYesterdayCustomers = await prisma.customer.findMany({
     where: {
       createdAt: {
-        gte: startOfYesterday, // 昨天 0 点（+8）对应的 UTC 开始时间
-        lte: endOfYesterday, // 昨天 23:59:59 对应的 UTC 结束时间
+        gte: startOfDayBeforeYesterday, // 前天 UTC 0 点
+        lte: endOfDayBeforeYesterday, // 前天 UTC 23:59:59
       },
     },
   });
-  //产品数
-  const totalProducts = await prisma.product.findMany({});
 
+  // 获取今天、昨天和前天新增的产品数
   const todayProducts = await prisma.product.findMany({
     where: {
       created_at: {
-        gte: startOfToday, // 今天 0 点（+8）对应的 UTC 开始时间
+        gte: startOfToday, // 今天 UTC 0 点
         lte: now, // 当前时间的 UTC
       },
     },
   });
-  const yesdayProducts = await prisma.product.findMany({
+
+  const yesterdayProducts = await prisma.product.findMany({
     where: {
       created_at: {
-        gte: startOfYesterday,
-        lte: endOfYesterday,
+        gte: startOfYesterday, // 昨天 UTC 0 点
+        lte: endOfYesterday, // 昨天 UTC 23:59:59
       },
     },
   });
 
-  //订单数
-  const orders = await prisma.order.findMany({});
-  // 总销售额
+  const totalProducts = await prisma.product.findMany();
+  const totalCustomers = await prisma.customer.findMany();
+  // 订单和总销售额
+  const totalOrders = await prisma.order.findMany({});
   const totalSales = await prisma.order.aggregate({
     _sum: {
       totalAmount: true, // 计算 totalAmount 的总和
     },
   });
-  const totalCustomers = await prisma.customer.findMany();
 
-  // 查询今天的访客
+  // 查询今天的访客数
   const todayVisitors = await prisma.visitor.count({
     where: {
       createdAt: {
-        gte: startOfToday, // 今天0点（+8）对应的 UTC 开始时间
+        gte: startOfToday, // 今天 UTC 0 点
         lte: now, // 当前时间的 UTC
       },
     },
   });
 
-  // 查询昨天的访客
+  // 查询昨天的访客数
   const yesterdayVisitors = await prisma.visitor.count({
     where: {
       createdAt: {
-        gte: startOfYesterday, // 昨天 0 点（+8）对应的 UTC 开始时间
-        lte: endOfYesterday, // 昨天 23:59:59 对应的 UTC 结束时间
+        gte: startOfYesterday, // 昨天 UTC 0 点
+        lte: endOfYesterday, // 昨天 UTC 23:59:59
       },
     },
   });
 
-  // 查询前天的访客
+  // 查询前天的访客数
   const dayBeforeYesterdayVisitors = await prisma.visitor.count({
     where: {
       createdAt: {
-        gte: startOfDayBeforeYesterday, // 前天 0 点（+8）对应的 UTC 开始时间
-        lte: endOfDayBeforeYesterday, // 前天 23:59:59 对应的 UTC 结束时间
+        gte: startOfDayBeforeYesterday, // 前天 UTC 0 点
+        lte: endOfDayBeforeYesterday, // 前天 UTC 23:59:59
       },
     },
   });
@@ -141,23 +136,27 @@ const AdminPage = async () => {
           <Card bordered={false}>
             <Statistic title="产品总数" value={totalProducts.length} valueStyle={{ color: "#3f8600" }} />
             <div className="flex justify-between">
-              <Statistic title="昨日产品数" value={yesdayProducts.length} valueStyle={{ color: "#3f8600" }} />
+              <Statistic title="昨日产品数" value={yesterdayProducts.length} valueStyle={{ color: "#3f8600" }} />
               <Statistic title="今日产品数" value={todayProducts.length} valueStyle={{ color: "#3f8600" }} />
             </div>
             <Statistic
               title="新增产品数"
-              value={todayCustomers.length - yesdayCustomers.length}
+              value={todayProducts.length - yesterdayProducts.length}
               valueStyle={{ color: "#3f8600" }}
             />
           </Card>
         </Col>
         <Col>
           <Card bordered={false}>
-            <Statistic title="昨日客户数" value={yesdayCustomers.length} valueStyle={{ color: "#3f8600" }} />
-            <Statistic title="今日客户数" value={todayCustomers.length} valueStyle={{ color: "#3f8600" }} />
+            <Statistic
+              title="前日客户数"
+              value={dayBeforeYesterdayCustomers.length}
+              valueStyle={{ color: "#3f8600" }}
+            />
+            <Statistic title="昨日客户数" value={yesterdayCustomers.length} valueStyle={{ color: "#3f8600" }} />
             <Statistic
               title="新增客户数"
-              value={todayCustomers.length - yesdayCustomers.length}
+              value={yesterdayCustomers.length - dayBeforeYesterdayCustomers.length}
               valueStyle={{ color: "#3f8600" }}
             />
           </Card>
@@ -165,7 +164,7 @@ const AdminPage = async () => {
         <Col>
           <Card bordered={false}>
             <Statistic title="总客户数" value={totalCustomers.length} valueStyle={{ color: "#3f8600" }} />
-            <Statistic title="订单总数" value={orders.length} valueStyle={{ color: "#3f8600" }} />
+            <Statistic title="订单总数" value={totalOrders.length} valueStyle={{ color: "#3f8600" }} />
             <Statistic
               title="销售总金额"
               value={totalSales._sum.totalAmount ? totalSales._sum.totalAmount : 0}
